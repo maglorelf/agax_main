4ce456528453890657b405c70e81b1e3d952288a
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2025-11-17 13:16:25 +0100

    feat: Existing web

---
+++ src/galerias/photogallery/photo6965/_vti_cnf/sldshow.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|25 May 2009 02:06:10 -0000
+vti_author:SR|Che1\\Che
+vti_modifiedby:SR|Che1\\Che
+vti_nexttolasttimemodified:TW|25 May 2009 02:06:10 -0000
+vti_timecreated:TR|25 May 2009 02:15:00 -0000
+vti_lineageid:SR|{C95FA0FC-FA45-4B74-B84D-454CDB1B97A0}
+vti_cacheddtm:TX|25 May 2009 02:15:02 -0000
+vti_filesize:IR|4362
+vti_extenderversion:SR|5.0.2.6790
+vti_backlinkinfo:VX|
