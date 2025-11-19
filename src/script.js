225afff3127a9dd66b70c725022aea88cf4d3610
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2025-11-19 22:26:43 +0100

    feat: Cleanup of code

---
+++ src/script.js
@@ -0,0 +1,7 @@
+/**
+ * AGAX Modern Web - JavaScript
+ * Funcionalidad interactiva
+ */
+
+// Log de inicialización
+console.log('%c AGAX Web Moderna v1.0.0 ', 'background: #006699; color: white; font-weight: bold; padding: 5px;');
